b7cff3e0d88e7bae5932e1fb2df31d4985f8c920
thecodeplanner <thecodeplanner@gmail.com>
2021-01-12 18:41:18 -0500

    commit tree 0d231bee74646c98d142585cd65676a1df822d7b

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+
+function Header({mode, onDarkModeClick}) {
+    return (
+        <header>
+            <h2>Shopster</h2>
+                <button onClick={onDarkModeClick}>
+                {mode ? "Dark" : "Light"} Mode
+                </button>
+        </header>
+    )
+}
+
+export default Header;
